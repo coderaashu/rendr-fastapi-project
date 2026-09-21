18bd96d9755e8d77b83826e9b8f7a381aa5c9bbc
coderaashu <oneaashish@gmail.com>
2026-09-21 23:22:44 +0530

    updated database.py to use env variables

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker ,declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:bhendi@localhost:5434/blog_db"
 
+# 1. Try to get the URL from environment variables. Fallback to local for testing.
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:bhendi@localhost:5432/blog_db")
+
+# 2. Render gives "postgres://", but SQLAlchemy needs "postgresql://"
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+# 3. Create the engine using the final URL
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(bind=engine)
-
 Base = declarative_base()
